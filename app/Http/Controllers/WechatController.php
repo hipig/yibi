abda2cd3c024c6acabdb0110bc8930213f5870c2
hipig <205270006@qq.com>
2024-01-05 11:10:12 +0800

    优化消息接收

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -13,21 +13,26 @@ class WechatController extends Controller
         $app = app('wechat.official_account');
         $server = $app->getServer();
 
-        $server->with(function($message, \Closure $next) {
-            switch ($message->MsgType) {
-                case 'event':
-                    if ($message->Event === 'subscribe') {
-                        return '感谢您关注 一笔记账';
-                    }
-                    break;
-                case 'text':
-                    return '收到文本消息';
-                default:
-                    return '收到其它消息';
-            }
+//        $server->with(function($message, \Closure $next) {
+//            switch ($message->MsgType) {
+//                case 'event':
+//                    if ($message->Event === 'subscribe') {
+//                        return '感谢您关注 一笔记账';
+//                    }
+//                    break;
+//                case 'text':
+//                    return '收到文本消息';
+//                default:
+//                    return '收到其它消息';
+//            }
+//
+//            return $next($message);
+//        });
 
-            return $next($message);
+        $server->addMessageListener('text', function($message, \Closure $next) {
+            return '收到文本消息';
         });
+
 
         return $server->serve();
     }
